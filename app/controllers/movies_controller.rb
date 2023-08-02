5064a13278a279f1ea7bfb25ff970107eef093c1
jbenzer <136658052+jbenzer@users.noreply.github.com>
2023-08-02 13:28:15 +0000

    add edit form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,27 @@
 class MoviesController < ApplicationController
+  def update
+    m_id = params.fetch("the_id")
+    # look up exisiting record
+    matching_records= Movie.where({id: m_id})
+    the_movie = matching_records.at(0)
+
+    # overwrite each column with the values from user inputs
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    # save
+
+    the_movie.save
+
+    # redirect to the movie details page
+
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
